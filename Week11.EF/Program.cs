ac41d1cc5286779fb20a23fc6b0d97785aa6fdc4
Dariya Takenova <n01633069@humber.ca>
2024-03-31 09:36:31 -0400

    Add project files.

---
+++ Week11.EF/Program.cs
@@ -0,0 +1,19 @@
+﻿using Week11.EF.Models.Entities;
+
+public class Try
+{
+    static void Main(string[] args)
+    {
+        var dbContext = new MMABooksContext();
+
+        var customers = dbContext.Customers;
+
+        foreach (var customer in customers)
+
+        {
+
+            Console.WriteLine("The name is : " + customer.Name);
+
+        }
+    }
+}
